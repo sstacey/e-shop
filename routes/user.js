380887f0ca9707b18242cc597f508a9a8d9480b9
sstacey <shaun.stacey1@gmail.com>
2021-08-02 05:28:21 -0500

    Add GET user by id

--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,14 @@ const db = require('../db')
 const router = new Router()
 
 router.get('/', async (req, res) => {
-    const { rows } = await db.query("SELECT * FROM customer")
-    res.send(rows)
+    const { rows } = await db.query("SELECT * FROM users")
+    res.json(rows)
+})
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const { rows } = await db.query('SELECT * FROM users where id = $1', [id])
+    res.send(rows[0])
 })
 
 module.exports = router
