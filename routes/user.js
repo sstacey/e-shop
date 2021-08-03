b6414efae73d18881fcc001b667b5ff235fc9b1c
sstacey <shaun.stacey1@gmail.com>
2021-08-03 16:47:44 -0500

    Clean up user routes and add PUT route

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,20 @@
 const Router = require('express-promise-router')
 const bcrypt = require('bcrypt')
+const format = require('pg-format')
 
 const db = require('../db')
 
 const router = new Router()
+
+router.param('id', async (req, res, next) => {
+    const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [req.params.id])
+    if (rows[0]){
+        req.user = {...rows[0], password: ''}
+        next()
+    } else {
+        res.status(404).send()
+    }
+})
 
 router.get('/', async (req, res) => {
     const { rows } = await db.query("SELECT * FROM users")
@@ -11,24 +22,37 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
-    const { id } = req.params
-    const { rows } = await db.query('SELECT * FROM users where id = $1', [id])
-    res.send(rows[0])
+    res.send(req.user)
 })
 
 router.post('/', async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         const query = {
-            text: 'INSERT INTO users VALUES(DEFAULT, $1, $2);',
+            text: 'INSERT INTO users VALUES(DEFAULT, $1, $2) RETURNING *;',
             values: [req.body.email, hashedPassword]
         }
-        await db.query(query)
-        res.status(201).send()
+        const newUser = await db.query(query)
+        res.status(201).send(newUser.rows[0])
     } catch (e) {
         res.status(500).send(e.message)
     }
-    
+})
+
+router.put('/:id', async (req, res) => {
+    const updates = req.body
+    if (updates.password) {
+        const hashedPassword = await bcrypt.hash(updates.password, 10)
+        updates.password = hashedPassword
+    }
+    try {
+        for (let key in updates) {
+            await db.query(format('UPDATE users SET %I = %L WHERE id = %L',key, updates[key], req.user.id))
+        }
+        res.status(204).send()
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
 })
 
 module.exports = router
