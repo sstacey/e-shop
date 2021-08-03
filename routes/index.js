54bb14d1b6bf862c5d9795305e8660151ca34c7b
sstacey <shaun.stacey1@gmail.com>
2021-08-03 06:05:21 -0500

    Add orders and orders_item to DB

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const users = require('./user')
 const products = require('./products')
+const orders = require('./orders')
 
 module.exports = app => {
     app.use('/users', users)
     app.use('/products', products)
+    app.use('/orders', orders)
 }
---
+++ routes/orders.js
@@ -0,0 +1,13 @@
+const Router = require('express-promise-router')
+
+const db = require('../db')
+
+const router = new Router()
+
+router.get('/', aysnc (req, res) => {
+    
+})
+
+
+module.exports = router
+
