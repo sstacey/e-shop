4c2ba0e2e4f3967fb6382007150ae2b8092737de
sstacey <shaun.stacey1@gmail.com>
2021-08-03 06:20:00 -0500

    Add GET order and orders endpoints

--- routes/orders.js
+++ routes/orders.js
@@ -4,9 +4,41 @@ const db = require('../db')
 
 const router = new Router()
 
-router.get('/', aysnc (req, res) => {
-    
+
+router.param('id', async (req, res, next) => {
+    const query = {
+        text: 'SELECT * FROM orders WHERE id = $1',
+        values: [req.params.id]
+    }
+
+    const { rows } = await db.query(query)
+
+    if (rows[0]) {
+        req.order = rows[0]
+        next()
+    } else {
+        res.status(404).send()
+    }
 })
+
+router.get('/', async (req, res) => {
+    try {
+        const { rows } = await db.query('SELECT * FROM orders ORDER BY id')
+        res.send(rows)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    res.send(req.order)
+})
+
+router.get('/:id/items', async (req, res) => {
+    res.send()
+})
+
+
 
 
 module.exports = router
