e3eccf0f842800f25bab4523899ca71d06af27e5
sstacey <shaun.stacey1@gmail.com>
2021-08-11 05:25:17 -0500

    Add auth signup test creates new user

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -4,9 +4,10 @@ const app = require('../app')
 describe('Auth Endpoints', () => {
   it('should signup a new user', async () => {
     const res = await request(app).post('/signup').send({
-      email: 'testuser1@example.com',
+      email: 'testuser3@example.com',
       password: 'password',
     })
     expect(res.statusCode).toEqual(201)
+    expect(res.body).toHaveProperty('user')
   })
 })
