12e0a2b2702b7afd1e7f51e77420afd34f2a5487
sstacey <shaun.stacey1@gmail.com>
2021-08-12 14:01:54 -0500

    Add carts migration and seeds

--- src/server/db/migrations/20210812061516_carts.js
+++ src/server/db/migrations/20210812061516_carts.js
@@ -1,3 +1,16 @@
-exports.up = function (knex) {}
+exports.up = function (knex) {
+  return knex.schema.createTable('carts', (table) => {
+    table.increments('id')
+    table
+      .integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+  })
+}
 
-exports.down = function (knex) {}
+exports.down = function (knex) {
+  return knex.schema.dropTable('carts')
+}
